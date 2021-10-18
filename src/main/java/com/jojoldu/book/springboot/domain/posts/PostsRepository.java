35c888f7a8f6b217a422c478cd4121874e9a3773
gkdud583 <qqaa5533@naver.com>
2021-10-18 23:09:48 +0900

    JPA까지

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.jojoldu.book.springboot.domain.posts.Posts;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+}
